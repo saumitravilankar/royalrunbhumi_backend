209e38343998c4f26b12e741d7569acd74fb3831
saumitra <saumitra.vilankar77@gmail.com>
2023-04-13 14:21:40 +0530

    Cors changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/booking", require("./routes/slotRoutes"));
